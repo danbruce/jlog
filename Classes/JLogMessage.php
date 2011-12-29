03eda3772fdb0f3fe199b3f3a6375dab09eb6357
Daniel Bruce <daniel.bruce@mediamiser.com>
2011-12-29 09:56:45 -0500

    JLogMessage now supports different message levels.

--- Classes/JLogMessage.php
+++ Classes/JLogMessage.php
@@ -2,16 +2,26 @@
 
 class JLogMessage
 {
+
+	const FATAL   = 0;
+	const ERROR   = -10;
+	const WARNING = -20;
+	const NOTICE  = -30;
+
 	public $transaction;
 	public $contents;
+	public $errorLevel;
 	private $_fullMessage;
 
-	public function __construct($t, $c)
+	public function __construct($t, $c, $l = JLogMessage::WARNING)
 	{
 		$this->transaction = $t->id;
 		$this->contents = $c;
+		$this->errorLevel = $l;
 		$this->_fullMessage = array(
-			'contents' => $this->contents
+			'transaction' => $this->transaction,
+			'level'       => $this->errorLevel,
+			'contents'    => $this->contents
 		);
 	}
 
