9f8810532d3872ec7c3fe0aba4c7bec457a74509
Daniel Bruce <danny@decantedmedia.com>
2014-02-17 15:33:43 -0500

    Fixed autoload and unit test bootstrap.

--- JLog/autoload.php
+++ JLog/autoload.php
@@ -1,7 +1,11 @@
 <?php
-
+/**
+    @file JLog/autoload.php
+    @brief Contains the JLog namespace autoloader.
+    @codeCoverageIgnore
+ */
 spl_autoload_register(function ($className) {
-    $file = './'.str_replace('\\', DIRECTORY_SEPARATOR, $className).'.php';
+    $file = __DIR__.'/../'.str_replace('\\', DIRECTORY_SEPARATOR, $className).'.php';
 
     if (!file_exists($file)) {
         return false;
